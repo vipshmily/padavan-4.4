c70312f3790ecb15947d51b6edeb6e268f7be0aa
TürBö <6028824+TurBoTse@users.noreply.github.com>
2024-05-19 09:39:39 +0800

    Added a small fix to the refresh and apply  buttons

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ This project is based on original rt-n56u with latest mtk 4.4.198 kernel, which 
 - Added ccache support, may save up to 50%+ build time
 - Upgraded the toolchain and libc:
   - gcc 10.5.0
-  - uClibc-ng 1.0.48
+  - musl 1.2.5 / uClibc-ng 1.0.48
  - Enabled kernel cgroups support
  - Fixed K2P led label names
  - Replaced udpxy with msd_lite
--- trunk/user/www/n56u_ribbon_fixed/state.js
+++ trunk/user/www/n56u_ribbon_fixed/state.js
@@ -204,11 +204,6 @@ function showSystemInfo(cpu_now,force){
 	else
 		$j('#wifi5_b').removeClass('btn-info');
 
-	if ('<% nvram_get_x("", "sdns_enable"); %>' == '1')
-		$j('#button_script2').addClass('btn-info');
-	else
-		$j('#button_script2').removeClass('btn-info');
-
 	if(parseInt(sysinfo.wifi2.guest) > 0)
 		$j('#wifi2_b_g').addClass('btn-info');
 	else
@@ -230,7 +225,6 @@ var enabled2Gclass = '<% nvram_match_x("","rt_radio_x", "1", "btn-info"); %>';
 var enabled5Gclass = '<% nvram_match_x("","wl_radio_x", "1", "btn-info"); %>';
 var enabledGuest2Gclass = '<% nvram_match_x("","rt_guest_enable", "1", "btn-info"); %>';
 var enabledGuest5Gclass = '<% nvram_match_x("","wl_guest_enable", "1", "btn-info"); %>';
-var enabledBtnCommit = '<% nvram_match_x("","nvram_manual", "0", "display:none;"); %>';
 
 // L3 = The third Level of Menu
 function show_banner(L3){
@@ -368,8 +362,8 @@ function show_banner(L3){
 	bc += '    <td><a href="/Advanced_FirmwareUpgrade_Content.asp"><span id="firmver" class="time"></span></a></td>\n';
 	bc += '  </tr>\n';
 	bc += '  <tr>\n';
-	bc += '    <td><button type="button" id="commit_btn" class="btn btn-mini" style="width: 114px; height: 21px; outline:0; '+enabledBtnCommit+'" onclick="commit();"><i class="icon icon-fire"></i>&nbsp;<#CTL_Commit#></button></td>\n';
-	bc += '    <td><button type="button" id="freememory_btn" class="btn btn-mini" style="height: 21px; outline:0;" title="<#BTN_FREEMEMORY#>" onclick="freememory();"><i class="icon icon-trash"></i></button><button type="button" id="logout_btn" class="btn btn-mini" style="height: 21px; outline:0;" title="<#t1Logout#>" onclick="logout();"><i class="icon icon-user"></i></button> <button type="button" id="reboto_btn" class="btn btn-mini" style="height: 21px; outline:0;" title="<#BTN_REBOOT#>" onclick="reboot();"><i class="icon icon-repeat"></i></button><button type="button" id="shutdown_btn" class="btn btn-mini" style="margin-left: 15px; height: 21px; outline:0;" title="<#BTN_SHUTDOWN#>" onclick="shutdown();"><i class="icon icon-off"></i></button></td>\n';
+	bc += '    <td><input type="button" class="btn btn-info" value=<#CTL_refresh#> onClick="parent.location.reload();"> <input type="button" class="btn btn-primary" value="<#CTL_apply#>" onclick="applyRule();"></td>\n';
+	bc += '    <td><button type="button" id="freememory_btn" class="btn btn-mini" style="height: 21px; outline:0;" title="<#BTN_FREEMEMORY#>" onclick="freememory();"><i class="icon icon-trash"></i></button> <button type="button" id="logout_btn" class="btn btn-mini" style="height: 21px; outline:0;" title="<#t1Logout#>" onclick="logout();"><i class="icon icon-user"></i></button> <button type="button" id="reboto_btn" class="btn btn-mini" style="height: 21px; outline:0;" title="<#BTN_REBOOT#>" onclick="reboot();"><i class="icon icon-repeat"></i></button> <button type="button" id="shutdown_btn" class="btn btn-mini" style="margin-left: 15px; height: 21px; outline:0;" title="<#BTN_SHUTDOWN#>" onclick="shutdown();"><i class="icon icon-off"></i></button></td>\n';
 	bc += '  </tr>\n';
 	bc += '</table>\n';
 	bc += '</div>\n';
@@ -847,7 +841,7 @@ function logout(){
 }
 
 function reboot(){
-	if(!confirm('<#Main_content_Login_Item7#>'))
+	if(!confirm('<#JS_reboot#>'))
 		return;
 	showLoading(board_boot_time());
 	var $j = jQuery.noConflict();
@@ -873,7 +867,6 @@ function freememory(){
 	$j.post('/apply.cgi',
 	{
 		'action_mode': ' FreeMemory ',
-		'current_page': 'index.asp'
 	});
 }
 
